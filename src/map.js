c0a9b6f509dd73ff139a1a4969a755878b7f0f46
davidboivin <dave.a.boivin@gmail.com>
2021-05-03 13:50:34 -0500

    Noticed an intermittent map display issue on the app hosted my website, so I fixed it. snuck in some code cleanup

--- src/map.js
+++ src/map.js
@@ -3,16 +3,20 @@ import MapboxDraw from "@mapbox/mapbox-gl-draw";
 import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 import turfArea from '@turf/area';
 
+const TOKEN = 'pk.eyJ1IjoiZGF2aWQtYiIsImEiOiJja28wZWt6M2YwY3d5MnBvaWs2c3gyejFrIn0.QESsL61dLc3e-AIfKi6k8w';
+const PAGE_MAP_ID = 'map';
+
 // mapbox setup
-mapboxgl.accessToken = 'pk.eyJ1IjoiZGF2aWQtYiIsImEiOiJja28wZWt6M2YwY3d5MnBvaWs2c3gyejFrIn0.QESsL61dLc3e-AIfKi6k8w';
+mapboxgl.accessToken = TOKEN;
 
 var map = new mapboxgl.Map({
-    container: 'map', // container id
+    container: PAGE_MAP_ID,
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [-71.0782263, 42.3816688],
-    zoom: 12 // starting zoom
+    zoom: 12
 });
 
+// setup geocode search
 map.addControl(new mapboxgl.NavigationControl());
 map.addControl(
     new MapboxGeocoder({
@@ -21,6 +25,7 @@ map.addControl(
     })
 );
 
+// setup polygon draw
 var draw = new MapboxDraw({
     displayControlsDefault: false,
     controls: {
@@ -29,12 +34,16 @@ var draw = new MapboxDraw({
     },
     defaultMode: 'draw_polygon'
 });
-
 map.addControl(draw);
-
 map.on('draw.create', updateArea);
 map.on('draw.delete', updateArea);
 map.on('draw.update', updateArea);
+
+// seems to fix the intermittent map display issue on page load
+var mapDiv = document.getElementById(PAGE_MAP_ID);
+if (mapDiv.style.visibility === true) {
+    map.resize();
+}
 
 // calculates area of the drawn polygon then determines the total solar panels and nominal power for the given area
 // updates the html elements with calculations
